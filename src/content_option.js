72984d27ec542d6e120aa71d8a4891359d78b720
Victory <blazehoodz@gmail.com>
2024-04-23 14:29:04 +0100

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+const logotext = "VICTORY CHUKWUEMEKA";
+const meta = {
+    title: "Victory Chukwuemeka",
+    description: "I’m Victory Chukwuemeka, a Java Developer ",
+};
+
+const introdata = {
+    title: "I’m Victory Chukwuemeka",
+    animated: {
+        first: "I love coding",
+        second: "I code Java/SpringBoot",
+        third: "I develop integrated and secure backend System",
+        fourth: "I am the one you seek for the Job,"
+    },
+    description: "",
+    your_img_url: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/365815778_5779209855513939_2821730805622339031_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=yi_ji-lcQhQAX_VDamY&_nc_ht=scontent-los2-1.xx&oh=00_AfDdSWL9hy29tN6SydgsSj1oQpwuVSrCl8BN8ivO58s5zw&oe=656D30E1",
+};
+
+const dataabout = {
+    title: "SUMMARY",
+    aboutme: "An enthusiastic and skilled developer with expertise in designing, installing, testing, and maintaining software systems. Proficient in crafting high-quality code tailored for fast-paced businesses, in line with the latest technological advancements. Experienced in technologies like Java, Spring Boot, Struts2, JPA, MySQL, Postgres, Entity Manager, Git workflow, and other cutting-edge development tools. Capable of managing independent projects effectively while also thriving in collaborative team settings. Possesses deep knowledge in Data Structures, Software Architecture, Test Driven Development, and Agile methodology, showcasing proficiency across all project phases from design to deployment. Eager to collaborate with inspiring individuals to create exceptional solutions.",
+};
+const worktimeline = [{
+        jobtitle: "REMOTE",
+        where: "",
+        date: "GLOBAL",
+    },
+    {
+        jobtitle: "HYBRID",
+        where: "",
+        date: "GLOBAL",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "JAVA",
+        value: 100,
+    },
+    {
+        name: "SPRING BOOT",
+        value: 100,
+    },
+    {
+        name: "STRUTS2",
+        value: 80,
+    },
+    {
+        name: "MYSQL / POSTGRES",
+        value: 100,
+    },
+    {
+        name: "GIT, CI/CD",
+        value: 90,
+    },
+    // {
+    //     name: "KAFKA ",
+    //     value: 100,
+    // },
+    {
+        name: "TDD",
+        value: 100,
+    },
+    {
+        name: "SWAGGER/POSTMAN",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Backend Developer",
+        description: "As a seasoned Backend Developer with a focus on Java, Spring Boot, and Struts2, I specialize in architecting the foundational elements of digital platforms. My innovative approach enables me to craft server-side solutions that power high-performance applications. My proficiency in Java, Spring Boot, and Struts2 empowers me to develop scalable backend architectures, including the creation of robust RESTful APIs. I skillfully manage relational databases such as MySQL and PostgreSQL to ensure data integrity and efficiency. Additionally, I leverage NoSQL databases like MongoDB to accommodate diverse data structures. Collaboration is central to my working style as I collaborate closely with DevOps teams to facilitate continuous integration and deployment. I possess strong troubleshooting skills, enabling me to identify and resolve bottlenecks while implementing new features with an emphasis on performance and scalability. Dedicated to staying abreast of industry trends and best practices, I remain updated on security standards and the latest advancements in backend technologies."
+    },
+    // {
+    //     title: "Mobile Apps",
+    //     description: "As a skilled Mobile Developer specializing in Flutter, I excel in creating cross-platform applications that seamlessly blend functionality. From design to implementation, I ensure a superior user experience, translating ideas into polished mobile solutions.\n\nDedicated to staying current with industry trends, innovative applications. "
+    // },
+    // {
+    //     title: "",
+    //     description: "",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://res.cloudinary.com/drqql45ms/image/upload/v1713877078/liqhub_copy_mozo2y.jpg",
+        description: "LiquorHub: An online store for various types of drinks |Java,Spring,HTML|",
+        link: "https://github.com/Victoryc01/LiquorHub.git",
+    },
+    {
+        img: "https://res.cloudinary.com/drqql45ms/image/upload/v1713875926/fashionblog.png",
+        description: "Fashion Blog API: For designers to showcase designs, receive feedback via comments and likes |Java,Spring|",
+        link: "https://github.com/Victoryc01/Fashion-Blog-API.git",
+    },
+    {
+        img: "https://res.cloudinary.com/drqql45ms/image/upload/v1713877046/SS_copy_n7xosy.jpg",
+        description:  "Swift Select: e-Hiring web app written with Java and React |Java,Spring,React|",
+        link: "https://github.com/Victoryc01/Swift-Select.git",
+
+    },
+    {
+        img: "https://res.cloudinary.com/drqql45ms/image/upload/v1713875687/fleetapp.png",
+        description: "FleetApp: A car management web app |Java,Struts2,HTML| ",
+        link: "#",
+    },
+    // {
+    //     img: "  https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407372998_6182671415167779_3563559725909258730_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=CQBpJcM2fOgAX9Txx2d&_nc_ht=scontent-los2-1.xx&oh=00_AfAurJo3M7gW22aqhHUYrfYLe8lZ8ZGzyfiS9blKtgQZiQ&oe=656EEEC9      ",
+    //     description: "MobilFind core Java and Kotlin(mobile)",
+    //     link: "https://play.google.com/store/apps/details?id=com.decagon.mobifind",
+    // },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "victorychukwuemeka01@gmail.com",
+    YOUR_FONE: "+2348123574923",
+    description: "Thank you for reaching out. I'll make it a priority to respond promptly. Please note that the number +2348123574923 is my WhatsApp handle, and I consider it official. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_kmvi31l",
+    YOUR_TEMPLATE_ID: "template_m3js7nv",
+    YOUR_USER_ID: "R2NqiCUrRZZQ7CpKW",
+};
+
+const socialprofils = {
+    github: "https://github.com/victoryc01",
+    // facebook: "",
+    linkedin: "https://linkedin.com/in/victory-chukwuemeka-324821127",
+    // twitter: "",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
